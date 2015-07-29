7e3de23046dba79ee1be59540037d72365d775a5
Brad Dougherty <me@brad.is>
2015-07-28 23:59:05 -0400

    Update global npm modules

--- npm.sh
+++ npm.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+npm install -g caniuse-cmd
 npm install -g doge
 npm install -g eslint
 npm install -g gitjk
 npm install -g grunt-cli
+npm install -g karma-cli
+npm install -g npm-check
+npm install -g nsp
+npm install -g psi
+npm install -g sitespeed.io
 npm install -g tldr
-npm install -g keybase-installer
