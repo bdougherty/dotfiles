9455600911bfa308f9bcfe1a04fd2b3fffd20785
Brad Dougherty <me@brad.is>
2016-07-26 03:08:22 -0400

    Add a couple npm packages

--- npm.sh
+++ npm.sh
@@ -4,10 +4,12 @@ npm install -g caniuse-cmd
 npm install -g diff-so-fancy
 npm install -g doge
 npm install -g eslint
+npm install -g fast-cli
 npm install -g gitjk
 npm install -g grunt-cli
 npm install -g jsonlint
 npm install -g karma-cli
+npm install -g np
 npm install -g npm-check
 npm install -g nsp
 npm install -g psi
@@ -16,4 +18,5 @@ npm install -g remark-lint
 npm install -g sass-lint
 npm install -g sitespeed.io
 npm install -g tldr
+npm install -g trash-cli
 npm install -g updtr
