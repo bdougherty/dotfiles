c70aca72335afb7ad2dc038799f0be701e5c7982
Brad Dougherty <me@brad.is>
2019-10-30 17:05:53 -0400

    Add github action to run shellcheck

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -30,7 +30,7 @@ fi
 # Enable colors
 export CLICOLOR=1
 if [ -x /usr/bin/dircolors ]; then
-	test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+	(test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)") || eval "$(dircolors -b)"
 	alias ls='ls --color=auto'
 	alias grep='grep --color=auto'
 	alias fgrep='fgrep --color=auto'
